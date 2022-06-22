675dea7e28840e467cd48190f1a1887876bb74b2
ortayamix <ncpyolcu@gmail.com>
2022-06-22 22:30:49 +0300

    backend

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id:1,
+        name: "iPhone 12",
+        desc: "4.1-inch display",
+        price: 699,
+        image: "https://github.com/chaoocharles/complete-shopping-cart/blob/main/backend/images/iphone12.jpg?raw=true"
+    },
+    {
+        id:2,
+        name: "iPhone 12 Pro",
+        desc: "6.3-inch display",
+        price: 999,
+        image: "https://github.com/chaoocharles/complete-shopping-cart/blob/main/backend/images/iphone12pro.jpg?raw=true"
+    },
+    {
+        id:1,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://raw.githubusercontent.com/chaoocharles/complete-shopping-cart/main/backend/images/galaxyS.png"
+    },    
+]
+
+module.exports = products
